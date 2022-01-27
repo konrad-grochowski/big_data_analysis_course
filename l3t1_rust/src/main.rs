17c2b8b31213c8db42ff126e4a58878b6447e85d
konrad-grochowski <konrad7g@gmail.com>
2022-01-27 01:02:55 +0100

    Modify CLI for the last list

--- l3t1_rust/src/main.rs
+++ l3t1_rust/src/main.rs
@@ -1,6 +1,5 @@
 use async_recursion::async_recursion;
 use futures::join;
-use html2text;
 use itertools::Itertools;
 use rand::{seq::IteratorRandom, SeedableRng};
 use regex::Regex;
@@ -9,6 +8,9 @@ use std::collections::{HashMap, HashSet};
 use std::io::{Result, Write};
 use std::writeln;
 use std::{fs, fs::File};
+use futures::prelude::*;
+use futures::future::{join_all};
+use std::sync::Mutex;
 #[macro_use]
 extern crate lazy_static;
 
@@ -52,7 +54,7 @@ async fn initial_crawl(article_title: String, remaining_iter: usize) -> Vec<(Str
 
 async fn crawl(initial_article: String, iter_number: usize) -> Result<String> {
     let mut links_hash_map: HashMap<String, Vec<String>> =
-        initial_crawl(initial_article, iter_number - 1)
+        initial_crawl(initial_article.to_owned(), iter_number - 1)
             .await
             .into_iter()
             .collect();
@@ -62,7 +64,7 @@ async fn crawl(initial_article: String, iter_number: usize) -> Result<String> {
         strings.sort_unstable();
         strings.dedup();
     });
-    let filename = format!("./crawl_data.txt",);
+    let filename = format!("./{}.txt",initial_article);
     let mut file = File::create(&filename)?;
     for (key, values) in links_hash_map {
         for value in values {
@@ -134,11 +136,10 @@ fn page_rank(filename: String, iters_num: usize) -> Vec<String> {
             .map(|(key, value)| (key, 0.15 + 0.85 * value))
             .collect();
     }
-    println!("{:#?}", ranks);
     // assert_eq!(dbg!(ranks.keys().len()),hashmap.keys().len());
-    ranks
+    dbg!(ranks
         .into_iter()
-        .sorted_by(|left, right| (&right.1).partial_cmp(&left.1).unwrap())
+        .sorted_by(|left, right| (&right.1).partial_cmp(&left.1).unwrap()))
         .map(|(k, _)| k.to_string())
         .collect()
 }
@@ -158,19 +159,19 @@ fn analyze_article(filename: String, analyzed_article: String) {
         },
     );
     inv_links_map.get(analyzed_article.as_str());
+    let pointing_pages = inv_links_map[analyzed_article.as_str()].to_owned();
+    println!("Pages that have link to {}: {:#?}",analyzed_article,pointing_pages);
+    println!("There were {} such pages",pointing_pages.len());
+    let links_per_page = links_map.values().map(|links| links.len()).sum::<usize>() as f64 / links_map.len() as f64;
+    println!("Number of links per page: {:?}",links_per_page);
 }
 
 async fn search_engine(sorted_links: Vec<String>) -> HashMap<String, Vec<String>> {
     let word_regex: Regex = Regex::new(r#"\w+"#).unwrap();
-    let mut words_hash_map: HashMap<String, Vec<String>> = HashMap::new();
-    for link in sorted_links {
-        let body = get(format!("{}{}", LINK, link))
-            .await
-            .unwrap()
-            .text()
-            .await
-            .unwrap();
-        println!("3");
+    let words_hash_map: Mutex<HashMap<String, Vec<String>>> = Mutex::new(HashMap::new());
+    let bodies_futures= sorted_links.iter().map(|link| get(format!("{}{}", LINK, link)).then(|body| body.unwrap().text()));
+    let bodies= join_all(bodies_futures).await.into_iter().map(|result| result.unwrap()).collect::<Vec<_>>();
+    sorted_links.into_iter().zip(bodies.into_iter()).for_each( |(link,body)|{
         let mut words: Vec<_> = word_regex
             .captures_iter(&body)
             .map(|c| c.get(0).unwrap().as_str().to_string())
@@ -178,24 +179,63 @@ async fn search_engine(sorted_links: Vec<String>) -> HashMap<String, Vec<String>
             .collect();
         words.dedup();
         for word in words {
+
             words_hash_map
+                .lock()
+                .unwrap()
                 .entry(word)
                 .or_default()
                 .push(link.to_owned())
         }
-    }
-    words_hash_map
+    } );
+    // for (link, body) in  {
+    //     // let body = get(format!("{}{}", LINK, link))
+    //     //     .await
+    //     //     .unwrap()
+    //     //     .text()
+    //     //     .await
+    //     //     .unwrap();
+       
+    // }
+    words_hash_map.into_inner().unwrap()
 }
 #[tokio::main]
 async fn main() -> Result<()> {
-    let initial_article = "Jesus".to_string();
-    let crawl_iter_number = 512;
-    let filename = dbg!(crawl(initial_article, crawl_iter_number).await.unwrap());
-    let page_rank_iter_number = 100;
-    let sorted_links = page_rank(filename, page_rank_iter_number);
+    let action = std::env::args().nth(1).expect("Provide action");
+    let title = std::env::args().nth(2).expect("Provide title");
 
-    let words_hash_map = search_engine(sorted_links).await;
-    println!("{:#?}", words_hash_map["potential"]);
+    match action.as_str() {
+        "crawl" => {
+            
+            let crawl_iter_number = 200;
+            let _filename = dbg!(crawl(title, crawl_iter_number).await.unwrap());
+        },
+        "pagerank" | "search" | "analyze" => {
+            let page_rank_iter_number = 100;
+            let sorted_links = page_rank(title.to_owned(), page_rank_iter_number);
+            println!("{:#?}",sorted_links);
+            if action == "search" {
+                let word_to_search = std::env::args().nth(3).expect("Provide word to search");
+                let words_hash_map = search_engine(sorted_links).await;
+                println!("{:?}",words_hash_map[&word_to_search])
+            }
+            else if action == "analyze" {
+                let word_to_analyze = std::env::args().nth(3).expect("Provide word to analyze");
+                analyze_article(title, word_to_analyze)
+            }
+        },
+
+        _ => panic!("Incorrect action")
+
+    }
+    // let initial_article = "Jesus".to_string();
+    // let crawl_iter_number = 512;
+    // let filename = dbg!(crawl(initial_article, crawl_iter_number).await.unwrap());
+    // let page_rank_iter_number = 100;
+    // let sorted_links = page_rank(filename, page_rank_iter_number);
+
+    // let words_hash_map = search_engine(sorted_links).await;
+    // println!("{:#?}", words_hash_map["potential"]);
     Ok(())
 
 }
